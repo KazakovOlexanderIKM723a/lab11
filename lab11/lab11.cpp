0a8017d35d39b781d0de0c3d062b31456f6cf1d5
Олександр Казаков <Oleksandr.Kazakov@infiz.khpi.edu.ua>
2024-06-09 16:06:51 +0300

    Добавьте файлы проекта.

---
+++ lab11/lab11.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    
+    double a, b;
+    cout << "Enter the value of variable a: ";
+    cin >> a;
+    cout << "Enter the value of variable b: ";
+    cin >> b;
+
+    double* pa = &a;
+    double* pb = &b;
+
+    if (*pa > *pb) {
+        *pa += 7;  
+        *pb -= 3;  
+    }
+    else {
+        *pb += 7;  
+        *pa -= 3;  
+    }
+
+    cout << "Value of variable a: " << a << endl;
+    cout << "Value of variable b: " << b << endl;
+
+    return 0;
+}
+
